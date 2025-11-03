fe628cb39522aeeb4ff2a57c1b4d05a68b18f8af
Mangalam Mishra <mangalamab17@gmail.com>
2025-11-03 14:10:40 +0530

    Basic project setup

---
+++ frontend/interviewmate/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewMate from "./pages/InterviewMate/InterviewMate";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {/* {default Route} */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-mate/:sessionId"
+            element={<InterviewMate />}
+          />
+        </Routes>
+      </Router>
+
+    </div>
+  );
+};
+
+export default App;
